08c3e3abd2f6d18e2aa2116b82ddd069913e5820
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2024-09-16 08:53:37 +1000

    adds entity for employee

---
+++ backend/employee/src/main/java/io/nology/employee/employee/Employee.java
@@ -0,0 +1,16 @@
+package io.nology.employee.employee;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+}
