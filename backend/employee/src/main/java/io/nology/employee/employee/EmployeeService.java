d6e1aebeea78a090fba702580da14d78eece3b5f
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2024-09-20 15:48:53 +1000

    adds updateEmployeeById to employeeservice

--- backend/employee/src/main/java/io/nology/employee/employee/EmployeeService.java
+++ backend/employee/src/main/java/io/nology/employee/employee/EmployeeService.java
@@ -42,4 +42,49 @@ public class EmployeeService {
         return this.repo.findById(id);
     }
 
+    public Optional<Employee> updateEmployeeById(Long id, @Valid UpdateEmployeeDTO data) {
+        Optional<Employee> result = this.findById(id);
+        if (result.isEmpty()) {
+            return result;
+        }
+        Employee foundEmployee = result.get();
+        if (data.getFirstName() != null) {
+            foundEmployee.setFirstName(data.getFirstName());
+        }
+        if (data.getLastName() != null) {
+            foundEmployee.setLastName(data.getLastName());
+        }
+        if (data.getEmail() != null) {
+            foundEmployee.setEmail(data.getEmail());
+        }
+        if (data.getPhone() != null) {
+            foundEmployee.setPhone(data.getPhone());
+        }
+        if (data.getAddress() != null) {
+            foundEmployee.setAddress(data.getAddress());
+        }
+        if (data.getSuburb() != null) {
+            foundEmployee.setSuburb(data.getSuburb());
+        }
+        if (data.getState() != null) {
+            foundEmployee.setState(data.getState());
+        }
+        if (data.getPostcode() != null) {
+            foundEmployee.setPostcode(data.getPostcode());
+        }
+        if (data.getRole() != null) {
+            foundEmployee.setRole(data.getRole());
+        }
+        if (data.getEmploymentType() != null) {
+            foundEmployee.setEmploymentType(data.getEmploymentType());
+        }
+        if (data.getStartDate() != null) {
+            foundEmployee.setStartDate(data.getStartDate());
+        }
+        if (data.getEndDate() != null) {
+            foundEmployee.setEndDate(data.getEndDate());
+        }
+        Employee updatedEmployee = this.repo.save(foundEmployee);
+        return Optional.of(updatedEmployee);
+    }
 }
